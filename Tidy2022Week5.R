36936ae181568bdbd6218123879a2100301395fb
Rachel Roday <77400912+rroday@users.noreply.github.com>
2022-02-02 10:22:59 -0600

    Update Tidy2022Week5.R

--- Tidy2022Week5.R
+++ Tidy2022Week5.R
@@ -6,7 +6,7 @@
 ##
 ## ---------------------------
 ##
-## Notes: Time to code: 45 min :/
+## Notes: Time to code: 110 min :/
 ##        
 ##
 ## ---------------------------
@@ -18,23 +18,48 @@ library(tidytuesdayR)
 library(dplyr)
 library(ggplot2)
 library(tidyr)
+library(stringr)
+library(showtext)
 
 #Go! Get! Your! Data!
-breed_traits <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/breed_traits.csv')
-trait_description <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/trait_description.csv')
-breed_rank_all <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/breed_rank.csv')
+breed_traits <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/breed_traits.csv') %>%
+  mutate(Breed = str_squish(Breed))
+breed_rank_all <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/breed_rank.csv') %>%
+  mutate(Breed = str_squish(Breed)) 
 
-# Im tired and dont know why left_join isnt working
+font_add_google(name = "Bebas Neue", family = "bebas")
+showtext_auto()
+
+
 breed_rank_all %>%
   mutate(sum_rank = rowSums(across(where(is.numeric)))) %>%
   filter(sum_rank <= 50) %>%
   left_join(breed_traits, by = "Breed") %>%
-  select(-starts_with('Coat')) %>%
+  select(-'Coat Type') %>%
+  select(-'Coat Length') %>%
   pivot_longer(cols = 'Affectionate With Family':'Mental Stimulation Needs', names_to = 'Category', values_to = 'Value') %>%
-  ggplot(aes(x=Breed, y=Value, colour = Category))+
-  geom_point(position=position_dodge(width=.3), alpha=.5)+
-  theme_bw()+
-  theme(axis.text.x = element_text(angle = 45, vjust=.5))+
-  labs(title="Top Ranked Dog Breeds from 2013-2020 and Their Best Qualities",
-       caption= "Data: American Kennel Club | Viz: @rachel_roday")
+  ggplot(aes(x="", y=Value, fill=Category)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0) +
+  facet_wrap(~Breed)+
+  labs(fill = "Breed Traits",
+       title="Which Dog breed Should My Family adopt?",
+       subtitle = "Top Ranked Dog Breeds from 2013-2020 and Their Best Traits (from 1 to 5)",
+       caption= "Data: American Kennel Club | Viz: @rachel_roday",
+       x="",
+       y="") +
+  theme(plot.background = element_rect(fill = "#D7BAAA", colour="#D7BAAA"),
+        panel.background = element_rect(fill = "#D7BAAA", colour="#D7BAAA"),
+        legend.background = element_rect(fill = "#D7BAAA", colour="#D7BAAA"),
+        legend.position = "right",
+        plot.title = element_text(colour = 'white', family="bebas", face = "bold", size=30),
+        plot.subtitle = element_text(colour = 'white', family="bebas", size=15),
+        plot.caption = element_text(colour = '#717370', family="bebas", size=15),
+        legend.text = element_text(colour = 'black', family="bebas", size=10), 
+        legend.title = element_text(colour = 'black', family="bebas", size=20),
+        strip.background =element_rect(fill="#D7BAAA"),
+        strip.text = element_text(colour = 'black', family="bebas", size=20),
+        axis.ticks = element_blank(),
+        panel.grid = element_blank(),
+        axis.text =  element_text(colour = 'black', family="bebas", size=10))
 
